e28eebf0b614fd258583c6db6ae82a1ee770d57a
pcharlot <pcharlot@student.21-school.ru>
2021-01-26 03:20:26 +0300

    just touching:

--- src/main/scala/Hello_name.scala
+++ src/main/scala/Hello_name.scala
@@ -4,5 +4,7 @@ object Hello_name extends App {
 
   val name = StdIn.readLine("Hi! What's your name?\n")
 
-  println("Nice to meet you, " + name + "!")
+  println("Nice to meet you, " + name + "!") //конкатенация
+
+  println(s"Great to meet you, $name! Just using interpolation") //интерполяция
 }
